e4130235dee9004b17c026f169b1a655f1eca534
Fabien Delecroix <fabien.delecroix@univ-lille.fr>
2021-11-11 12:16:02 +0100

    tests refactorés

--- game/j5e/game/TestMove.py
+++ game/j5e/game/TestMove.py
@@ -7,36 +7,30 @@ class TestMove(unittest.TestCase):
     def setUp(self):
         self.gs = GameSpace()
         self.gs.init_graph(3, 24, 12)
-
-    def test_strip00_to_ring01(self):
-        start = ("line", 0, 0, 0)
-        goal = ("ring", 0, 1, 8)
+        
+    def go(self,start,goal):
         agent=Lemming(1, "Lemmiwings",start, 1, goal, self.gs)
         turns=0
         while agent.active and turns<100:
             agent.go()
             turns+=1
-        self.assertEqual (goal,agent.pos)
+        self.assertEqual(goal,agent.pos)
 
+    def test_strip00_to_ring01(self):
+        start = ("line", 0, 0, 0)
+        goal = ("ring", 0, 1, 8)
+        self.go(start,goal)
+        
     def test_ring01_to_strip01(self):
         start = ("ring", 0, 1, 9)
         goal = ("strip", 0, 1, 0)
-        agent=Lemming(1, "Lemmiwings", start, 1, goal, self.gs)
-        turns=0
-        while agent.active and turns<100:
-            agent.go()
-            turns+=1
-        self.assertEqual (goal,agent.pos)
+        self.go(start,goal)
         
     def test_ring_21_to_strip20(self):
         start = ("ring", 2, 1, 3)
         goal = ("strip", 2, 0, 23)
-        agent=Lemming(1, "Lemmiwings", start, 1, goal, self.gs)
-        turns=0
-        while agent.active and turns<100:
-            agent.go()
-            turns+=1
-        self.assertEqual (goal,agent.pos)
+        self.go(start,goal)
+
 
 if __name__ == '__main__':
     unittest.main()
