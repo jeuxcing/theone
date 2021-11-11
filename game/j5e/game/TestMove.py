ff3e7d39fe447e26811ac146828fc82f7d10cb2d
Fabien Delecroix <fabien.delecroix@univ-lille.fr>
2021-11-11 14:36:11 +0100

    tests réparés et étendus

--- game/j5e/game/TestMove.py
+++ game/j5e/game/TestMove.py
@@ -8,29 +8,39 @@ class TestMove(unittest.TestCase):
         self.gs = GameSpace()
         self.gs.init_graph(3, 24, 12)
         
-    def go(self,start,goal):
+    def go(self,start,goal, turnsMax):
         agent=Lemming(1, "Lemmiwings",start, 1, goal, self.gs)
         turns=0
-        while agent.active and turns<100:
+        while agent.active and turns<turnsMax:
             agent.go()
             turns+=1
         self.assertEqual(goal,agent.pos)
 
-    def test_strip00_to_ring01(self):
+    def test_ring000_to_line001(self):
+        start = ("ring", 0, 0, 0)
+        goal = ("line", 0, 0, 1)
+        self.go(start,goal,100)
+
+    def test_line000_to_ring018(self):
         start = ("line", 0, 0, 0)
         goal = ("ring", 0, 1, 8)
-        self.go(start,goal)
+        self.go(start,goal,100)
         
-    def test_ring01_to_strip01(self):
+    def test_ring019_to_line010(self):
         start = ("ring", 0, 1, 9)
-        goal = ("strip", 0, 1, 0)
-        self.go(start,goal)
+        goal = ("line", 0, 1, 0)
+        self.go(start,goal,100)
         
-    def test_ring_21_to_strip20(self):
-        start = ("ring", 2, 1, 3)
-        goal = ("strip", 2, 0, 23)
-        self.go(start,goal)
+    def test_line010_to_col020(self):
+        start = ("line", 0, 1, 0)
+        goal = ("col", 0, 2, 0)
+        self.go(start,goal,100)
 
+    
+    def test_ring_213_to_line2023(self):
+        start = ("ring", 2, 1, 3)
+        goal = ("line", 2, 0, 12)
+        self.go(start,goal,100)
 
 if __name__ == '__main__':
     unittest.main()
