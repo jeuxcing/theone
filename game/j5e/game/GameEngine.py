8b64a1f8e8145c5df4e4d909302fea1f583e087c
fabien.delecroix <fabien.delecroix@univ-lille.fr>
2023-07-25 13:01:05 +0200

    first part of json externalization

--- game/j5e/game/GameEngine.py
+++ game/j5e/game/GameEngine.py
@@ -1,55 +1,32 @@
 from threading import Thread
 from multiprocessing import Event
+from os import path
 
 from game.j5e.game.GameExceptions import OutOfLedsException, NextLedOnOtherSegmentException
-from game.j5e.game.Level import Level
+from game.j5e.game.Level import Level, LevelBuilder
 from game.j5e.game.Agents import Actions
-
-
 
 
 class GameEngine(Thread):
 
-    def __init__(self, ):
+    def __init__(self):
         super().__init__()
         self.timer = Event()
-        self.level = Level(3)
-
+        self.levels = LevelBuilder.load_level_list_from_json(
+            path.join("game","j5e","game","levels","level_1.json"))
+        self.current_level_idx = 0
+        self.current_level = self.levels[0].copy()
 
     def add(self,el):
         self.elements.append(el)
-
-    def get_next_coord(self, coord, dir):
-
-        #Select the current segment
-        current_segment = None
-        if (coord.segment_type == SegType.RING):
-            current_segment = self.rings[coord.row][coord.col]
-        elif (coord.segment_type == SegType.ROW):
-            current_segment = self.rows[coord.row][coord.col]
-        else:
-            current_segment = self.cols[coord.row][coord.col]
-
-        #Get the next coord
-        try:
-            return current_segment.get_next_coord(coord, dir)
-        except OutOfLedsException:
-            raise OutOfLedsException            
-        except NextLedOnOtherSegmentException:
-            #TODO calcul coordonné en sortie de segment
-            if dir==Direction.FORWARD:
-                +1
-            else :
-                -1
-            return Coordinate()
 
         
     def run(self):
         i=0
         # boucle d'action des éléments de jeu
-        while (not self.level.is_over()) and (not self.timer.wait(0.2)):
+        while (not self.current_level.is_over()) and (not self.timer.wait(0.2)):
             print(' Tour n°', i,' : ')
-            for agent in self.level.agents:
+            for agent in self.current_level.agents:
                 action = agent.play()
                 match(action):
                     case Actions.MOVE:
@@ -58,16 +35,4 @@ class GameEngine(Thread):
                         print(agent.name," va en ", agent.segment.coord, agent.offset)
                          
             i+=1
-
-
-'''
-        try:
-            self.coord = self.coord.get_next_coord(self.dir)
-        except OutOfLedsException:
-            self.dir = self.dir.opposite()       
-            self.coord = self.coord.get_next_coord(self.dir)
-        if self.coord == self.goal:
-            self.active = False
-            print(self.name," a fini")
-        
-'''
+    
--- game/j5e/game/Level.py
+++ game/j5e/game/Level.py
@@ -1,3 +1,4 @@
+import json
 from game.j5e.game.Geometry import *
 from game.j5e.game.Agents import Lemming
 
@@ -17,92 +18,93 @@ class Level:
                         ]#,Lemming(2, "Octodon", self.rings[0][0], 0 , Direction.RING_CLOCKWISE)]
         self.agents = [l for l in self.lemmings]
 
-        self.rings[0][0].set_3h(self.rows[0][0])
+    def add_connection_to_ring(self, ring_row, ring_col, hour):
+        match hour:
+            case 3: 
+                self.rings[ring_row][ring_col].set_3h(self.rows[ring_row][ring_col])
+            case 6:
+                self.rings[ring_row][ring_col].set_6h(self.cols[ring_row][ring_col])
+            case 9:
+                self.rings[ring_row][ring_col].set_9h(self.rows[ring_row-1][ring_col])
+            case 12:
+                self.rings[ring_row][ring_col].set_12h(self.cols[ring_row][ring_col-1])        
 
     def is_over(self):
         return len(self.lemmings) == 0
 
-'''
-    gameEngine.add(Trap(3, "une porte",4))
+    def copy(self):
+        lvl = Level(self.grid_size)
+        for ring_row_idx, ring_row in enumerate(self.rings):
+            for ring_col_idx, ring in enumerate(ring_row):
+                for path_idx, path in enumerate(ring.paths):
+                    if path is not None:
+                        lvl.add_connection_to_ring(ring_row_idx, ring_col_idx, path_idx)
+        return lvl
 
+class LevelBuilder:
 
+    def __init__(self):
+        pass
 
+    def load_level_list_from_json(json_file_path):
+        #TODO plusieurs chemins de levels dans un fichier
+        return [LevelBuilder.load_level_from_json(json_file_path)]
 
-                for element in self.elements:
-                element.go()
-                # suppression des lemmings non actifs
-                if  isinstance(element,Lemming) and element.active==False:
-                    self.elements.remove(element)
-                    ## arrêt lorsque plus aucun lemming n'est actif
-                    if True not in [isinstance(ele,Lemming) for ele in self.elements] :
-                        self.timer.set()
+    def load_level_from_json(json_file_path):
+        json_level = json.loads(open(json_file_path).read())
+        grid_size = None
+        geometry = None
+        lemmings = None
+        objects = None
 
-
-
-
-
-    def set_config_from_json(self, json_file_path):
-        self.config.load_config_from_json(json_file_path)
-        self.translate_config_to_gamespace()
-        self.setup_agents()
-
-    # Create GameSpace graph matching the config
-    def translate_config_to_gamespace(self):
-        print("init graph with params", self.config.grid_size, self.config.n_leds_segment, self.config.n_leds_ring)
-        self.gamespace.init_graph(self.config.grid_size, self.config.n_leds_segment, self.config.n_leds_ring)
-        print("adding", len(self.config.walls), "walls")
-        for w in self.config.walls:
-            self.gamespace.set_section_status(w[0].segment_type, w[0].seg_pos_x, w[0].seg_pos_y, w[0].pos_in_seg, w[1].pos_in_seg, 0)
-        print("reversing direction on", len(self.config.walls), "rings")
-        for rr in self.config.reverse_rings:
-            self.gamespace.change_direction_segment(rr.segment_type, rr.seg_pos_x, rr.seg_pos_y)
-
-    def setup_agents(self):
-        self.agent=Lemming(1, "Lemmiwings",self.config.start_point.getCoord(), Direction.FORWARD, self.config.end_point.getCoord(), self.gamespace)
-        # TODO : setup from agents in config, only one for the moment
-        
-'''       
-
-
-
-
-
-
-
-class Config:
-
-    def __init__(self, grid_s = 3):
-        self.grid_size = grid_s
-        self.n_leds_segment = n_leds_s
-        self.n_leds_ring = n_leds_r
-        self.walls = []
-        self.reverse_rings = []
-        self.n_lemmings = 1
-        self.time_between_spawns_lemmings = 5
-        self._n_lemmings_to_win = 1
-
-    # Parser
-    def load_config_from_json(self, json_file_path):
-        json_contents = json.loads(open(json_file_path).read())
-        # Level layout
-        json_level = json_contents['level']
-        # Mandatory
+       # Mandatory
         try:
-            json_start_point = json_level['start_point']
-            json_end_point = json_level['end_point']
-            self.start_point = Position(json_start_point['segment_type'], json_start_point['seg_pos_x'], json_start_point['seg_pos_y'], json_start_point['pos_in_seg'])
-            self.end_point = Position(json_end_point['segment_type'], json_end_point['seg_pos_x'], json_end_point['seg_pos_y'], json_end_point['pos_in_seg'])
-        except AttributeError:
-            print("error : no valid start or end point found in json config file ", json_file_path)
+            grid_size = json_level['grid_size']
+            geometry = json_level['geometry']
+            lemmings = json_level['lemmings']
+            
+        except KeyError as e:
+            print("error : attribute not found in json config file ", e)
+            return None
+
+        lvl = Level(grid_size)
+        LevelBuilder.initialize_geomtry(lvl,geometry)
+        LevelBuilder.initialize_lemmings(lvl,lemmings)
+        return lvl
+
+    def initialize_geomtry(lvl, geometry):
+        try:
+            fully_connected = geometry['fully_connected']
+            ring_connections = geometry['ring_connections']
+            
+        except KeyError as e:
+            print("error : attribute not found in json config file ", e)
+            return None
+
+        #todo fully
+        for connection in ring_connections:
+            try:
+                lvl.add_connection_to_ring(
+                    connection['row_coord'], connection['col_coord'], connection['hour_coord'])
+            except KeyError as e:
+                print("error : attribute not found in json config file ", e)
+                return None
+
+
+
+    def initialize_lemmings(lvl, lemmings):
+        pass
+'''
+
         # Optional
         keyz = json_level.keys()
         for key in keyz:
             #print(key)
             if key == 'dimensions':
                 #print("found dimensions")
-                json_dimensions = json_level['dimensions']
-                if hasattr(json_dimensions, 'grid_size'):
-                    self.grid_size = json_dimensions['grid_size']
+                grid_size = json_level['dimensions']
+                if hasattr(grid_size, 'grid_size'):
+                    self.grid_size = grid_size['grid_size']
             elif key == 'hazards':
                 #print("found hazards")
                 json_hazards = json_level['hazards']
@@ -124,3 +126,28 @@ class Config:
                 self.n_lemmings = json_agents['lemmings']['number_spawns']
                 self.time_between_spawns_lemmings = json_agents['lemmings']['time_between_spawns']
                 self.n_lemmings_to_win = json_agents['lemmings']['number_to_win']
+
+'''
+
+'''
+    def set_config_from_json(self, json_file_path):
+        self.config.load_config_from_json(json_file_path)
+        self.translate_config_to_gamespace()
+        self.setup_agents()
+
+    # Create GameSpace graph matching the config
+    def translate_config_to_gamespace(self):
+        print("init graph with params", self.config.grid_size, self.config.n_leds_segment, self.config.n_leds_ring)
+        self.gamespace.init_graph(self.config.grid_size, self.config.n_leds_segment, self.config.n_leds_ring)
+        print("adding", len(self.config.walls), "walls")
+        for w in self.config.walls:
+            self.gamespace.set_section_status(w[0].segment_type, w[0].seg_pos_x, w[0].seg_pos_y, w[0].pos_in_seg, w[1].pos_in_seg, 0)
+        print("reversing direction on", len(self.config.walls), "rings")
+        for rr in self.config.reverse_rings:
+            self.gamespace.change_direction_segment(rr.segment_type, rr.seg_pos_x, rr.seg_pos_y)
+
+    def setup_agents(self):
+        self.agent=Lemming(1, "Lemmiwings",self.config.start_point.getCoord(), Direction.FORWARD, self.config.end_point.getCoord(), self.gamespace)
+        # TODO : setup from agents in config, only one for the moment
+
+       '''       
